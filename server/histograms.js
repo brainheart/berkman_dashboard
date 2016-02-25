d008e9cbcf25fe82df408dee9395bdd5054c80af
Reinhard Engels <reinhard.engels@gmail.com>
2016-02-25 16:59:02 -0500

    Histograms collection

---
+++ server/histograms.js
@@ -0,0 +1,47 @@
+Histogram = function(doc) {
+  return _.extend(this, doc);
+};
+
+_.extend(Histogram.prototype, {
+
+});
+
+Histograms = new Mongo.Collection('histograms', {
+  transform: function(doc) { return new Histogram(doc); }
+});
+
+var HistogramSchema = new SimpleSchema({
+  project: {
+    type: String,
+    max: 255,
+    optional: false
+  },
+  metric: {
+    type: String,
+    max: 255,
+    optional: false
+  },
+  start_date: {
+    type: Date,
+    optional: false
+  },
+  values : {
+    type: Number,
+    optional: false
+  },
+  granularity: {
+    type: String,
+    regEx: /hour|day|month|year|financial_year/,
+    optional: false
+  }
+});
+
+Histograms.attachSchema(HistogramSchema);
+
+Meteor.publish('histograms', function(project,metric,granularity) {
+  var selector = {project:project, metric: metric, granularity:granularity};
+  var options  = {fields: {start_date: 1, values: 1} };
+  var docs = Histograms.find(selector,options);
+  return docs;
+});
+
