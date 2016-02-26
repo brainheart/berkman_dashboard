a6d350d2e5ea021b655fa4f6f25fc99ff693e8a6
Reinhard Engels <reinhard.engels@gmail.com>
2016-02-26 15:48:02 -0500

    Simple charts of twitter follower counts actually work

--- client/templates/home.js
+++ client/templates/home.js
@@ -3,15 +3,12 @@
 Template.Home.onCreated(function() {
   console.log('Template.Home.onCreated');
   this.subscribe('projects');    
-  this.subscribe('scores',"berkmancenter","twitter_followers");
+  this.subscribe('scores');
 });
 
 
 Template.Home.helpers({
   reinos: "This is Reinhard",
-  old_projects: function(){
-    return [ {id: 'Project1' }, {id: 'Project2' }  ];
-  },
   projects: function(){
     return Projects.find();
   },
@@ -22,3 +19,46 @@ Template.Home.helpers({
 
 
 
+
+
+Template.Home.onRendered(function() {
+  
+  var template = this;
+  
+  console.log("Template.Home.onRendered");
+  
+  this.autorun(function() {
+
+    if (!template.subscriptionsReady()) {
+      return;
+    }    
+
+    var plotScores = function plotScores(project){
+      console.log('plotScores for project ', project);
+      var scores = [];
+      Scores.find({project:project.id}).forEach(function(s){
+        console.log("here is somthing for ya: " , s);
+        scores.push(s);//{ date: s.date, value:  s.value });
+        console.log("here is what it turns into: ", scores[-1]);
+      });
+      console.log("Hi Reinhard. You've almost figured your yesterday right thing out.");
+      MG.data_graphic({
+        title: "Downloads",
+        description: "This graphic shows a time-series of downloads.",
+        data: scores,
+        width: '600',
+        height: 255,
+        target: '#twitter_'+project.id,
+        x_accessor: 'date',
+        y_accessor: 'value',
+        min_y_from_data: true
+      });
+    };
+    
+    Projects.find().forEach(function(project){
+      console.log('examining project ' , project);
+      plotScores(project);
+    });
+
+  });
+});
--- server/publications.js
+++ server/publications.js
@@ -1,10 +1,11 @@
 
 
-Meteor.publish('scores', function(project,metric) {
-  var selector = {project:project, metric: metric};
-  var options  = {fields: {date: 1, value: 1} };
-  var docs = Scores.find(selector,options);
-  return docs;
+Meteor.publish('scores', function() {
+  return Scores.find();
+  //var selector = {metric: metric};
+  //var options  = {fields: {project: 1, metric: 1,date: 1, value: 1} };
+  //var docs = Scores.find(selector,options);
+  //return docs;
 });
 
 Meteor.publish("projects", function(){
